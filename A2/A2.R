194046a7364440086e41aeba9aba50d73d5e62c1
Xiang Xu <xxu@telaria.com>
2019-10-29 18:25:30 -0700

    A2

--- A2/A2.R
+++ A2/A2.R
@@ -4,6 +4,8 @@ library(GGally)
 library(lubridate)
 library(scales)
 
+setwd("/Users/valentina/Desktop/2019 fall/231/assignment/A2")
+
 # Step 6: Join Elastic Map Reduce output with precipitation data ---------------------------------
 
 EMRoutput = read_tsv("./EMRoutput.tsv", col_names = c("date", "hour", "drivers_onduty", "t_onduty", "t_occupied", "n_pass", "n_trip", "n_mile", "earnings"))
@@ -42,6 +44,14 @@ output$rainLevel[which(output$precip>=0.3)] = "Heavy Rain"
 
 # Average earning
 output$avgWage = output$earnings/output$drivers_onduty
+output$onduty_perc = output$t_onduty/output$drivers_onduty
+output$occupy_perc = output$t_occupied/output$drivers_onduty
+output %>% mutate (rain = factor(rain))
+ggplot(data = output, aes(x = hour, y = avgWage)) +
+  geom_boxplot(aes(fill=rain)) +
+  labs(x = "Time in a Day",  y = "Avg Earning per Driver", color = 'Rained Or Not', title = "Scatter plot average earning on each day")  +
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 raindf <- output %>% group_by(hour, rain) %>% summarize(avgWage = mean(avgWage)) %>% filter(rain==1)
 noraindf <- output %>% group_by(hour, rain) %>% summarize(avgWage = mean(avgWage)) %>% filter(rain==0)
@@ -51,16 +61,30 @@ colnames(plot.data) = c("hour", "avgWageRain","avgWageNoRain")
 
 plot.data %>% ggplot() + 
   geom_line(data = plot.data, aes(x = hour, y = avgWageRain, color = "Rain", group = 1)) +
+  geom_point(data = plot.data, aes(x = hour, y = avgWageRain, color = "Rain", group = 1)) +
   geom_line(data = plot.data, aes(x = hour, y = avgWageNoRain, color = "noRain", group = 1)) + 
   geom_point(data = plot.data, aes(x = hour, y = avgWageNoRain, color = "noRain", group = 1)) +
-  labs(x = "Time",  y = "average earning", color = 'Rained Or Not', title = "Average Wage by Hour")  +
-  theme_bw() + scale_color_manual(values = c('Rain' = 'blue','noRain' = 'red'))
+  labs(x = "Time in a Day",  y = "Avg Earning per Driver", color = 'Rained Or Not', title = "Average Wage by Hour, over 2010-2013")  +
+  theme_bw() + scale_color_manual(values = c('Rain' = 'blue','noRain' = 'red')) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-plot.data <- output %>% group_by()
+fm = lm(data = output, formula = output$avgWage ~ 1 + rain)
+print(summary(fm))
+print(summary(fm)$r.squared)
 
-output %>% ggplot() + 
-  geom_line(data = output, aes(x = timeInterval, y = FGame, color = "Female")) +
-  geom_line(data = output, aes(x = timeInterval, y = MGame, color = "Male")) + 
-  labs(x = "Time",  y = "tweet count", color = 'Type', title = "Tweet count over 1 day by gender, keyword: game")  +
-  theme_bw() + scale_color_manual(values = c('Female' = 'red','Male' = 'blue'))
-ggsave(plot=plotG, file="./gameTweetPlot.pdf", width=10, height=5)
+fm2 = lm(data = output, formula = output$avgWage ~ 1 + rainLevel)
+print(summary(fm2))
+print(summary(fm2)$r.squared)
+
+fm3 = lm(data = output, formula = output$onduty_perc ~ 1 + output$avgWage)
+print(summary(fm3))
+print(summary(fm3)$r.squared)
+
+fm4 = lm(data = output, formula = log(output$onduty_perc) ~ 1 + log(output$avgWage))
+print(summary(fm4))
+print(summary(fm4)$r.squared)
+  
+  
+  
+  
+
